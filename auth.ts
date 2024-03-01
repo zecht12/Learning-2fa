8b0f444332a649c3781d221e85daea542aad2665
zecht12 <gilangprimae13@gmail.com>
2024-03-01 13:19:48 +0700

    push

--- auth.ts
+++ auth.ts
@@ -29,7 +29,6 @@ export const {handlers: { GET, POST },auth,signIn,signOut} = NextAuth({
 
         if (!existingUser?.emailVerified) return false;
 
-
         if (existingUser.isTwoFactorEnabled) {
             const twoFactorConfirmation = await getTwoFactorConfirmationByUserId(existingUser.id);
             if (!twoFactorConfirmation) return false;
